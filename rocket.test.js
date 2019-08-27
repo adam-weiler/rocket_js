3509f00e9aeee152e82f3b2b638fecbf22227723
adam-weiler <adam.weiler@yahoo.ca>
2019-08-27 17:04:24 -0400

    Testing liftoff, if flying is true when initialized, and if rocket initialized as flying.

--- rocket.test.js
+++ rocket.test.js
@@ -6,36 +6,52 @@ describe('Rocket', () => {
   describe('constructor', () => {
     test('it should set default attributes if nothing is passed', () => {
       // arrange
-      // Rocket(); //In Python.
       let rocket = new Rocket();
 
       // assert
-      expect(rocket).toBeInstanceOf(Rocket);
+      expect(rocket.name).toBeTruthy();
+      expect(rocket.colour).toBeTruthy();
+      expect(rocket.flying).toEqual(false);
     });
 
 
-    // it('should set flying to true', () => {
-    //   rocket = new Rocket();
-    //   rocket.flying();
-    //   expect(rocket.fly).toBe(true);
-
-
-    test("it should set the rocket's name if provided", () => {
+    test("it should set the rocket's name, colour, and flying if provided", () => {
       // arrange
-      // let rocket = new Rocket('Apollo', 'Red', false);
-
-      let options = { name: 'Apollo' }
-      let rocket = new Rocket(options);
+      let rocket = new Rocket({ name: 'Apollo', colour: 'Red', flying: true });
 
       // assert
       expect(rocket.name).toEqual('Apollo');
+      expect(rocket.colour).toEqual('Red');
+      expect(rocket.flying).toEqual(true);
     });
   });
 
+  
   describe('liftOff', () => {
-    // ...
+    test('sets flying to true when initialized with default values', () => {
+      // arrange
+      let rocket = new Rocket();
+
+      // assert
+      rocket.liftOff();
+      expect(rocket.flying).toEqual(true);
+
+      rocket.liftOff()
+      expect(rocket.flying).toEqual(true);
+    });
+
+    test('returns false if the rocket is initialized as flying', () => {
+      // arrange
+      let rocket = new Rocket({ flying: true });
+
+      // assert
+      rocket.liftOff();
+      expect(rocket.liftOff()).toBe(false);
+      expect(rocket.flying).toEqual(true);
+    });
+
   });
 
-  // ...
+    
 
 });
