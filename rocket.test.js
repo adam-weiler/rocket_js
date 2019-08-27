289f963c4b5a2f37ac6dc2df9d7d0a04cabdb29e
adam-weiler <adam.weiler@yahoo.ca>
2019-08-27 16:21:09 -0400

    Testing if nothing is passed, testing if rocket's name is provided.

--- rocket.test.js
+++ rocket.test.js
@@ -5,11 +5,30 @@ describe('Rocket', () => {
 
   describe('constructor', () => {
     test('it should set default attributes if nothing is passed', () => {
-      // ...
+      // arrange
+      // Rocket(); //In Python.
+      let rocket = new Rocket();
+
+      // assert
+      expect(rocket).toBeInstanceOf(Rocket);
     });
 
+
+    // it('should set flying to true', () => {
+    //   rocket = new Rocket();
+    //   rocket.flying();
+    //   expect(rocket.fly).toBe(true);
+
+
     test("it should set the rocket's name if provided", () => {
-      // ...
+      // arrange
+      // let rocket = new Rocket('Apollo', 'Red', false);
+
+      let options = { name: 'Apollo' }
+      let rocket = new Rocket(options);
+
+      // assert
+      expect(rocket.name).toEqual('Apollo');
     });
   });
 
